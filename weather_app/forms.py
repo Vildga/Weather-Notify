d76a2202a5ee8450cb22a361d298cf20ca3673f6
Vildga <vildga.game@gmail.com>
2024-10-09 20:54:50 +0300

    added style to sign up and sign in page

--- weather_app/forms.py
+++ weather_app/forms.py
@@ -17,33 +17,16 @@ class SubscriptionForm(forms.ModelForm):
 
 
 class RegistrationForm(UserCreationForm):
-    first_name = forms.CharField(max_length=30, required=False, help_text='Optional.')
-    last_name = forms.CharField(max_length=30, required=False, help_text='Optional.')
-    email = forms.EmailField(max_length=254, help_text='Required. Enter a valid email address.')
+    email = forms.EmailField(required=True)
+    first_name = forms.CharField(max_length=30, required=True)
+    last_name = forms.CharField(max_length=30, required=True)
 
     class Meta:
         model = User
         fields = ('username', 'first_name', 'last_name', 'email', 'password1', 'password2')
-
-    def clean_username(self):
-        username = self.cleaned_data['username']
-        if User.objects.filter(username=username).exists():
-            raise forms.ValidationError(f'A user with the username {username} already exists.')
-        return username
-
-    def clean_password1(self):
-        password1 = self.cleaned_data.get('password1')
-
-        try:
-            validate_password(password1)
-        except ValidationError as e:
-            raise forms.ValidationError('\n'.join(e.messages))
-
-        return password1
-
-    def clean_email(self):
-        email = self.cleaned_data.get('email')
-        if User.objects.filter(email=email).exists():
-            raise forms.ValidationError(f'A user with the email {email} already exists.')
-        return email
+        help_texts = {
+            'username': None,
+            'password1': '',
+            'password2': '',
+        }
 
